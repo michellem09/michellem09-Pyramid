bcfa8ff93bfbe499781c8bd903eba456b16e26bc
Michelle Mendez <mmendez5584@gmail.com>
2023-03-03 09:08:55 +0000

    Pyramid Lab

--- src/main/java/Pyramid.java
+++ src/main/java/Pyramid.java
@@ -28,6 +28,15 @@ public class Pyramid {
      * @return a string representation of the pyramid.
      */
     public String returnPyramid(int n){
-        return "";
+        String p = "";
+
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= i; j++){
+                p += "*";
+            }
+            p+="\n";
+        }
+
+        return p;
     }
 }
